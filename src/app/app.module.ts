d9f1b68bac4ed0800ccae7871e6c65aacb4b5ac3
aanicod <desac100@gmail.com>
2021-10-19 21:00:06 +0530

    2nd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewCmpComponent } from './new-cmp/new-cmp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewCmpComponent
   ],
   imports: [
     BrowserModule,
