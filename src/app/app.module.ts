413b6f519bc6e0ad64f4fb7157b598750604e714
aanicod <desac100@gmail.com>
2021-10-19 22:14:13 +0530

    4th

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewCmpComponent } from './new-cmp/new-cmp.component';
-/*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*/
+
 @NgModule({
   declarations: [
     AppComponent,
